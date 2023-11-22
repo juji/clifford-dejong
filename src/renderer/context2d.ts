1a7aa2e98d9ffac1c2980839887b9ba2df3ab879
juji <jujiyangasli@gmail.com>
2023-11-22 12:47:31 +0700

    not perfect, but okay

--- src/renderer/context2d.ts
+++ src/renderer/context2d.ts
@@ -2,21 +2,37 @@
 import { type UIOptions } from '../ui'
 import { hsv2rgb } from './hsv2rgb'
 import { clifford, dejong } from './attractors'
+import BezierEasing from 'bezier-easing'
+
+// clifford
+// dejong
+const saturationBezier = BezierEasing(.79,-0.34,.54,1.18)
+const lightnesBezier = BezierEasing(.75,.38,.24,1.33)
+
 
 // smoothing comes before scale
 function smoothing(num: number, scale: number){
   return num + (Math.random()<0.5?-0.2:0.2) * (1/(scale))  
 }
 
-function getColorData(dx: number, dy:number, density: number, maxDensity: number) : number{
+function getColorData(
+  dx: number, 
+  dy: number, 
+  density: number, 
+  width: number,
+  height: number,
+  maxDensity: number,
+  h: number,
+  s: number
+) : number{
   
-  // console.log('hsv2rgb')
+  const mdens = Math.log(maxDensity)
+  const pdens = Math.log(density)
   const [ r, g, b ] = hsv2rgb(
-    360,
-    100,
-    100,
+    h,
+    s - saturationBezier(pdens / mdens) * s,
+    lightnesBezier(pdens / mdens) * 100,
   )
-  // console.log(rgb)
   
   return 255 << 24 | b << 16 | g << 8 | r
 }
@@ -30,8 +46,9 @@ export class Context2d {
   anim: number = 0
   scale: number = 150
   itt = 0
-  maxItt = 200
-  perItt = 40000
+  maxItt = 100
+  // perItt = 40000
+  perItt = 100000
 
   x: number[] = [0];
   y: number[] = [0];
@@ -52,9 +69,9 @@ export class Context2d {
 
   attractor: ((x:number,y:number,a:number,b:number,c:number,d:number) => number[])|null = null
   background = 0
-  green = 255<<24|0<<16|255<<8|0
   maxDensity = 0
   startat: Date
+  // green = 255<<24|0<<16|255<<8|0
 
   constructor( canvas: HTMLCanvasElement, options: UIOptions ){
     this.context = canvas.getContext('2d') as CanvasRenderingContext2D
@@ -100,9 +117,8 @@ export class Context2d {
     this.context.restore()
   }
 
-  onDone(){
+  drawBitmap(){
 
-    // this.context.translate( -this.width/2, -this.height/2 )
     let bitmap = this.context.createImageData(this.width, this.height);
     let buf = new ArrayBuffer(bitmap.data.length);
     let buf8 = new Uint8ClampedArray(buf);
@@ -116,7 +132,11 @@ export class Context2d {
             this.pixels[`${x},${y}`][0],
             this.pixels[`${x},${y}`][1],
             this.pixels[`${x},${y}`][2],
-            this.maxDensity
+            this.width,
+            this.height,
+            this.maxDensity,
+            this.options?.hue as number,
+            this.options?.saturation as number
           )
         ) : this.background;
       }
@@ -125,24 +145,26 @@ export class Context2d {
     bitmap.data.set(buf8);
     this.clear()
 	  this.context.putImageData(bitmap, 0,0);
-    this.context.beginPath();
-    this.context.rect(
-      0,
-      0, 
-      54, 54
-    );
-    this.context.fillStyle = `rgb(255,0,0)`
-    this.context.fill()
+
+    // red square
+    // this.context.beginPath();
+    // this.context.rect(
+    //   -29,
+    //   -29, 
+    //   54, 54
+    // );
+    // this.context.fillStyle = `rgb(255,0,0)`
+    // this.context.fill()
 
 
   }
 
   start(){
 
-    if(this.paused && this.itt>=1) return;
+    if(this.paused && this.itt>=20) return;
     if(this.itt >= this.maxItt) {
       console.log('before done', new Date().valueOf() - this.startat.valueOf() + 'ms')
-      this.onDone()
+      this.drawBitmap()
       return console.log('done', new Date().valueOf() - this.startat.valueOf() + 'ms');
     }
 
@@ -176,20 +198,18 @@ export class Context2d {
       a, b, c, d
     )
     
-    // const thisX = smoothing(x, this.scale) 
-    // const thisY = smoothing(y, this.scale)
     const thisX = smoothing(x, this.scale) 
     const thisY = smoothing(y, this.scale)
 
-    const realX = Math.round(thisX * this.scale)
-    const realY = Math.round(thisY * this.scale)
+    const screenX = Math.round(thisX * this.scale)
+    const screenY = Math.round(thisY * this.scale)
     
     // translate index to 0,0
-    // it wil lbe used to calculate bitmap
-    const index = `${realX + (this.width/2)},${realY + (this.height/2)}`
+    // it will be used to calculate bitmap
+    const index = `${screenX + (this.width/2)},${screenY + (this.height/2)}`
     if(!this.pixels[index]) this.pixels[index] = [0,0,0]
     this.pixels[index][0] += thisX - this.lastX
-    this.pixels[index][1] += thisY - this.lastY
+    this.pixels[index][1] += thisY - this.lastY //realY + (this.height/2)
     this.pixels[index][2] += 1
 
     if(this.maxDensity < this.pixels[index][2])
@@ -197,14 +217,16 @@ export class Context2d {
     
     this.lastX = thisX
     this.lastY = thisY
-    this.x.push(realX)
-    this.y.push(realY)
-
-
+    this.x.push(screenX)
+    this.y.push(screenY)
 
   }
 
   draw(){
+
+    if(!this.paused && (this.itt%50 === 0)){
+      this.drawBitmap()
+    }
 
     let n = 0;
     const ctx = this.context
@@ -215,7 +237,8 @@ export class Context2d {
         this.y[n], 
         1, 1
       );
-      ctx.fillStyle = `hsla(100,10%,40%, ${this.paused ? '1' : '0.1'})`
+      // ctx.fillStyle = `hsla(${this.options?.hue},${(this.options?.saturation||0)*.3}%,50%, ${this.paused ? '1' : '0.1'})`
+      ctx.fillStyle = `hsla(${this.options?.hue},${(this.options?.saturation||0)}%,50%, ${this.paused ? '1' : '0.1'})`
       ctx.fill()
       n++
     }
--- src/ui/lil-gui.ts
+++ src/ui/lil-gui.ts
@@ -7,6 +7,8 @@ export type Options = {
   b: number
   c: number
   d: number
+  hue: number
+  saturation: number
 }
 
 export default function optionPanel( 
@@ -17,11 +19,20 @@ export default function optionPanel(
   const gui = new GUI();
 
   const options:Options = {
-    attractor: 'clifford',
-    a: 2,
-    b: -2,
-    c: 1,
-    d: -1,
+    // attractor: 'clifford',
+    // a: 2,
+    // b: -2,
+    // c: 1,
+    // d: -1,
+    // hue: 333,
+    // saturation: 100
+    attractor: 'dejong',
+    a: -1.17,
+    b: 1.03,
+    c: 2.3,
+    d: -0.9,
+    hue: 333,
+    saturation: 100
   }
 
   gui.add( options, 'attractor', [
@@ -33,17 +44,22 @@ export default function optionPanel(
   gui.add( options, 'b', -3, 3);
   gui.add( options, 'c', -3, 3);
   gui.add( options, 'd', -3, 3);
+  gui.add( options, 'hue', 0, 360);
+  gui.add( options, 'saturation', 0, 100);
 
   gui.onChange( event => {
-    console.log('change')
     onChange(event.object as Options, true)
   })
 
   gui.onFinishChange( event => {
-    console.log('finish')
     onChange(event.object as Options, false)
   })
 
   initOptions( options )
+
+  // const colornode = document.createElement("div");
+  // colornode.classList.add('h-color')
+  // gui.$children.appendChild(colornode)
+  // console.log(gui.$children)
 
 }
