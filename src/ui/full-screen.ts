0a7e9256977c2ce78b2a30ebc1d64183a5ff2747
juji <jujiyangasli@gmail.com>
2023-11-24 19:22:15 +0700

    fix menu on full screen

--- src/ui/full-screen.ts
+++ src/ui/full-screen.ts
@@ -4,15 +4,20 @@ export function fullScreenButton(){
   
   const fullScreenButton = document.querySelector('button.full-screen') as HTMLElement
   const footer = document.querySelector('footer') as HTMLElement
+  const lilgui = document.querySelector('.lil-gui') as HTMLElement
   if(!fullScreenButton) return;
 
   fullScreenButton.addEventListener('click',() => {
-    fullScreenButton.classList.toggle('on')
-    footer.classList.toggle('full-screen')
 
     if(!document.fullscreenElement){
+      fullScreenButton.classList.add('on')
+      footer.classList.add('full-screen')
+      lilgui.classList.add('full-screen')
       document.documentElement.requestFullscreen();
     }else{
+      fullScreenButton.classList.remove('on')
+      footer.classList.remove('full-screen')
+      lilgui.classList.remove('full-screen')
       document.exitFullscreen();
     }
   })
