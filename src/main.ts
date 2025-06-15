c90ae84b5815dcfc72da2e2405d94d1f90e0156b
juji <jujiyangasli@gmail.com>
2025-06-15 15:49:46 +0700

    Revert "asdf"
    This reverts commit 6c8de0a81120da2a7886b49b4dad6038d62a4178.

--- src/main.ts
+++ src/main.ts
@@ -9,55 +9,6 @@ import Renderer from './renderer'
 import { ui } from './ui'
 const canvas = document.querySelector('canvas')
 
-// gem
-
-
-// coGem
-console.log(`
-  .--.
- |o_o |
- |:_/ |
-//   \\ \\\\
-(|     | )
-/'\\_   _/\`\\\\
-\\___)=(___/\n`);
-
-// coCld
-console.log(`
-    /\\_/\\
-   ( o.o )
-    > ^ <
-   /  |  \\
-   \\___|___/
-`);
-
-// coChat
-console.log(`\n   _____       _ _   _     _   \n  / ____|     (_) | | |   | |  \n | |     ___   _| |_| |__ | |_ \n | |    / _ \\ | | __| '_ \\| __|\n | |___| (_) || | |_| | | | |_ \n  \\_____\\___(_)_|\\__|_| |_|\\__|\n      GitHub Copilot\n`);
-
-// gem
-console.log(`
-  _.-""\`-._
- .'          \`.
-/   O      O   \\
-|    \\  ^^  /    |
-\\     \`----'     /
- \`. _______ .'
-   //_____\\\\
-`);
-
-
-
-// chat
-console.log(`
-  /\\_/\\
- ( o.o )  meow~
-  > ^ <
-`);
-
-
-// yes there's a difference
-
-
 if(canvas) {
   
   const footer = document.querySelector('footer') as HTMLElement
