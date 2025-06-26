75d6891c94b6fd3d13dd5862a1e61f4de80463ba
juji <jujiyangasli@gmail.com>
2025-06-27 02:59:35 +0700

    chore: initialize vitest

---
+++ turborepo/apps/web/src/__tests__/example.test.tsx
@@ -0,0 +1,9 @@
+
+import { render, screen } from '@testing-library/react';
+
+describe('Example', () => {
+  it('renders text', () => {
+    render(<div>Hello Test</div>);
+    expect(screen.getByText('Hello Test')).toBeInTheDocument();
+  });
+});
