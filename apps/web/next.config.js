e7125f30144c23f2e0fd08ab0fa6dbfb6460f8e5
juji <jujiyangasli@gmail.com>
2025-06-27 18:17:54 +0700

    fix next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,10 +2,11 @@ import { withTamagui } from "@tamagui/next-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  appDir: true,
+  
 };
 
 export default withTamagui({
   config: "./tamagui.config.ts",
   components: ["tamagui"],
+  appDir: true,
 })(nextConfig);
