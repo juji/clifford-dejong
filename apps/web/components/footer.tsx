201e641df49916890c8e703465915076ef9b8f13
juji <jujiyangasli@gmail.com>
2025-06-27 22:36:32 +0700

    learn: tailwind + chadcn

--- apps/web/components/footer.tsx
+++ apps/web/components/footer.tsx
@@ -6,9 +6,16 @@ export function Footer() {
   return (
      <footer className={cn(`
         fixed inset-x-0 bottom-0 w-full 
-        bg-background text-foreground/70
+        glass
+        text-white/70
         py-3 px-4 text-[0.8rem] font-light border-t 
-        z-[100] text-left border-foreground/20
+        z-[100] text-left border-0
+        before:content-[''] before:absolute 
+        before:top-[0px] before:h-[1px] 
+        before:width-full
+        before:left-0 before:right-0
+        before:bg-gradient-to-r
+        before:from-[#343434cf] before:to-[#ffffff63]
       `)}>
       <span>Clifford-de Jong Attractor &copy; {new Date().getFullYear()}</span>
     </footer>
