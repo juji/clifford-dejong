f68925945e10bb429ae7aa94374ce1535c85887c
juji <jujiyangasli@gmail.com>
2025-06-27 16:38:49 +0700

    bug: surpress react hydration warning

--- turborepo/apps/web/components/theme-toggle-button.tsx
+++ turborepo/apps/web/components/theme-toggle-button.tsx
@@ -2,11 +2,23 @@
 import { Button } from "tamagui";
 import { Sun, Moon } from "lucide-react";
 import { useThemeSetting, useRootTheme } from "@tamagui/next-theme";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggleButton() {
   const themeSetting = useThemeSetting();
   const [theme] = useRootTheme();
-  return (
+
+  const [draw, setDraw] = useState(false);
+
+  // This is a workaround for the Next.js hydration issue with Tamagui themes
+  // It ensures that the theme is set correctly on the client side
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setDraw(true);
+    }
+  }, []);
+
+  return draw ? (
     <Button
       size="$3"
       chromeless
@@ -21,5 +33,5 @@ export default function ThemeToggleButton() {
     >
       {theme === "dark" ? <Moon size={20} /> : <Sun size={20} />}
     </Button>
-  );
+  ) : null;
 }
