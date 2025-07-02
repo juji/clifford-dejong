774dd49098eff7f2a4e19198bb3b36f7120ae969
juji <jujiyangasli@gmail.com>
2025-07-02 15:23:41 +0700

    pwa: add and reference all standard Android, iOS, and Windows icons; update manifest and config for full PWA support

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,17 @@
+import withPWA from 'next-pwa';
 
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: false
-};
+const nextConfig = withPWA({
+  devIndicators: false,
+  pwa: {
+    dest: 'public',
+    disable: !isProd,
+    register: true,
+    skipWaiting: true,
+    // You can add more options here if needed
+  },
+});
 
-export default nextConfig
+export default nextConfig;
