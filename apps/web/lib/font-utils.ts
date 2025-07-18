0870c199ef5b4a0d5742d796254beed6e6fc17dc
juji <jujiyangasli@gmail.com>
2025-07-18 12:08:54 +0700

    remove some duplicate fonts, make the randomizer user larger number

--- apps/web/lib/font-utils.ts
+++ apps/web/lib/font-utils.ts
@@ -4,25 +4,19 @@
 const fonts = [
   'Alegreya',
   'Antonio',
-  'Cinzel',
-  'Gabarito',
+  'Bitcount',
   'Gluten',
   'Grenze Gotisch',
   'Merienda',
   'Nunito',
   'Open Sans',
-  'Playwrite DE Grund',
-  'Playwrite ID',
-  'Playwrite IN',
   'Playwrite NG Modern',
-  'Playwrite NL',
-  'Roboto Mono',
   'Shantell Sans',
   'Winky Rough'
 ];
 
 // Function to get a random font from the array
 export function getRandomFont(): string {
-  const randomIndex = Math.floor(Math.random() * fonts.length);
+  const randomIndex = Math.floor(Math.random() * (fonts.length * 333)) % fonts.length;
   return fonts[randomIndex] || 'Nunito'; // Fallback to Nunito if undefined
 }
