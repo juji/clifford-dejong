e68666711f6e26e556b5a3c9741facc4c7a1b6b2
juji <jujiyangasli@gmail.com>
2025-08-02 08:47:38 +0700

    chore(chaoscanvas): install react navigation

--- apps/chaoscanvas/android/app/src/main/java/com/jujiplay/chaoscanvas/MainActivity.kt
+++ apps/chaoscanvas/android/app/src/main/java/com/jujiplay/chaoscanvas/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.jujiplay.chaoscanvas
 
+/**
+* react navigation setting.
+*/
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+
+
 class MainActivity : ReactActivity() {
+
+  /**
+   * react navigation setting.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
