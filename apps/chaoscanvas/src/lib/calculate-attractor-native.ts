9e47802e0e35ecbee4a0932965a6de037a83e1b3
juji <jujiyangasli@gmail.com>
2025-08-04 09:54:17 +0700

    cancelation demo

--- apps/chaoscanvas/src/lib/calculate-attractor-native.ts
+++ apps/chaoscanvas/src/lib/calculate-attractor-native.ts
@@ -1,7 +1,7 @@
 import NativeAttractorCalc from '@specs/NativeAttractorCalc';
 
 // Define a constant for the buffer size for clarity
-const POINT_COUNT = 1000000;
+const POINT_COUNT = 2048 * 2048;
 // Assuming each point is represented by 4 int (r, g, b, a)
 const BYTES_PER_POINT = 4 * 4;
 const BUFFER_SIZE = POINT_COUNT * BYTES_PER_POINT;
@@ -45,7 +45,6 @@ export function calculateAttractorNative(
       );
     });
 
-  const now = new Date().getTime();
   const { promise, cancel } = NativeAttractorCalc.calculateAttractor(
     params.timestamp,
     sharedBuffer, // Pass the buffer here
@@ -53,6 +52,7 @@ export function calculateAttractorNative(
     onUpdate,
   ) as { promise: Promise<string>; cancel: () => void };
 
+  const now = new Date().getTime();
   setTimeout(() => {
     // If you want to cancel the calculation after some time, you can call cancel.
     console.log('Cancelling calculation after 500ms');
