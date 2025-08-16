e1571326bfbdf89915c2b8b3fadb6c6ff409a10b
juji <jujiyangasli@gmail.com>
2025-08-17 04:53:10 +0700

    cpp smoothing factor needs more

--- apps/chaoscanvas/shared/NativeAttractorCalc.cpp
+++ apps/chaoscanvas/shared/NativeAttractorCalc.cpp
@@ -241,7 +241,7 @@ NativeAttractorCalc::getLowQualityPoint(double hue, double saturation, double br
 
 double
 NativeAttractorCalc::smoothing(double num, double scale) {
-  const double factor = 0.2;
+  const double factor = 0.222;
   // Use C++ random to match JavaScript's Math.random() < 0.5 behavior
   return num +
     (static_cast<double>(std::rand()) / RAND_MAX < 0.5 ? -factor : factor) * (1.0 / scale);
